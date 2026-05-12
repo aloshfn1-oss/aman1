dfa334b75b12cd5975bc61ceb1efbf97b2b4a193
aloshfn1-oss <alosh.fn1@gmail.com>
2026-05-12 05:57:34 +0300

    as

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 
 const port = process.env.PORT || 10000;
   await app.listen(port, '0.0.0.0');
-  new Logger('Bootstrap').log(API running on port: ${port})
+  new Logger('Bootstrap').log(API running on port: ${port});
 }
 bootstrap();
