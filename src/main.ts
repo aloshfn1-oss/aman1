9d88dfc748d513f4e0f26818f69f5949888b29a4
aloshfn1-oss <alosh.fn1@gmail.com>
2026-05-12 05:51:17 +0300

    Change server port to 10000 and update log message
    Removed unnecessary semicolon from the log statement.

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,8 @@ async function bootstrap() {
     .build();
   SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, cfg));
 
- const port = process.env.PORT || 10000;
+const port = process.env.PORT || 10000;
   await app.listen(port, '0.0.0.0');
-  new Logger('Bootstrap').log(API running on port: ${port});
+  new Logger('Bootstrap').log(API running on port: ${port})
 }
 bootstrap();
